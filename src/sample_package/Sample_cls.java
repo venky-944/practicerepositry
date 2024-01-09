492fcd66213c7b4660e7c0fe47a93c3809ffc034
venky-944 <venkatnasana944@gmail.com>
2024-01-09 10:02:10 +0530

    first commit

---
+++ src/sample_package/Sample_cls.java
@@ -0,0 +1,11 @@
+package sample_package;
+
+public class Sample_cls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi venkybuh");
+
+	}
+
+}
---
+++ src/sample_package2/Sample_cls2.java
@@ -0,0 +1,11 @@
+package sample_package2;
+
+public class Sample_cls2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello pavan");
+
+	}
+
+}
